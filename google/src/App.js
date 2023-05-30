37d7b0c578824d41e20ac4aa3c37c0ebb709169c
Wizat <passavit1@gmail.com>
2023-05-30 10:58:24 +0700

    finish Header nav

--- google/src/App.js
+++ google/src/App.js
@@ -1,21 +1,42 @@
 import styled from "styled-components";
 import profile from "./Image/Profile.jpg";
+import logoWhite from "./Image/LogoWhite.png";
 
 const StyledContainer = styled.div`
   height: 100vh;
   color: white;
   box-sizing: border-box;
+  padding: 6px;
 
   .header {
     height: 5%;
-    outline: 0.5px red solid;
     display: flex;
     justify-content: flex-end;
     align-items: center;
 
+    div:first-child,
+    div:nth-child(2) {
+      padding-left: 15px;
+    }
+
     .menu {
-      width: 10%;
       height: 100%;
+      padding-right: 0.5%;
+      padding-bottom: 0.2%;
+      margin-left: 0.68%;
+
+      svg {
+        height: 100%;
+      }
+    }
+
+    .profile {
+      margin-right: 0.65%;
+      padding-top: 0.505%;
+
+      img {
+        border-radius: 50%;
+      }
     }
   }
 
@@ -44,24 +65,29 @@ function App() {
         <div className="menu">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="-10 -11 180 100"
+            viewBox="-12 -11.8 47 47"
             fill="rgb(226,226,227)"
           >
-            <circle cx="7" cy="7" r="1.8"></circle>
-            <circle cx="13" cy="7" r="1.8"></circle>
-            <circle cx="19" cy="7" r="1.8"></circle>
-            <circle cx="7" cy="13" r="1.8"></circle>
-            <circle cx="13" cy="13" r="1.8"></circle>
-            <circle cx="19" cy="13" r="1.8"></circle>
-            <circle cx="7" cy="19" r="1.8"></circle>
-            <circle cx="13" cy="19" r="1.8"></circle>
-            <circle cx="19" cy="19" r="1.8"></circle>
+            <circle cx="7" cy="7" r="1.85"></circle>
+            <circle cx="13" cy="7" r="1.85"></circle>
+            <circle cx="19" cy="7" r="1.85"></circle>
+            <circle cx="7" cy="13" r="1.85"></circle>
+            <circle cx="13" cy="13" r="1.85"></circle>
+            <circle cx="19" cy="13" r="1.85"></circle>
+            <circle cx="7" cy="19" r="1.85"></circle>
+            <circle cx="13" cy="19" r="1.85"></circle>
+            <circle cx="19" cy="19" r="1.85"></circle>
           </svg>
+        </div>
+        <div className="profile">
           <img src={profile} alt="" />
         </div>
+
         <div></div>
       </div>
-      <div className="main">Main</div>
+      <div className="main">
+        <img src={logoWhite} alt="" />
+      </div>
       <div className="country">Country</div>
       <div className="footer">Footer</div>
     </StyledContainer>
